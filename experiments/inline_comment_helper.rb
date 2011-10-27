a0333f76105c5c4320e383e34410907f613410b5
Alex Bredariol Grilo <alex@fonista.co>
2011-10-27 17:55:49 -0200

    Search results commments

---
+++ experiments/inline_comment_helper.rb
@@ -0,0 +1,4 @@
+ab_test 'Inline comment helpers' do
+  alternatives :none, :inline
+  metrics :inline_click_search_result, :inline_upvoted, :inline_commented
+end
---
+++ experiments/metrics/inline_commented.rb
@@ -0,0 +1,3 @@
+metric 'Inline click search result' do
+  description 'Tracks the influence of inline comment on search result comment'
+end
