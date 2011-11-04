5fb68c6c9b9538a56173b337727e99cd81424525
Alex Bredariol Grilo <alex@fonista.co>
2011-11-04 17:36:18 -0200

    Fix metric names

--- experiments/metrics/inline_commented.rb
+++ experiments/metrics/inline_commented.rb
@@ -1,3 +1,3 @@
-metric 'Inline click search result' do
+metric 'Inline commented' do
   description 'Tracks the influence of inline comment on search result comment'
 end
--- experiments/metrics/inline_upvoted.rb
+++ experiments/metrics/inline_upvoted.rb
@@ -1,3 +1,3 @@
-metric 'Inline click search result' do
+metric 'Inline upvoted' do
   description 'Tracks the influence of inline comment on search result upvotes'
 end
