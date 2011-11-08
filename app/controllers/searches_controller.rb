fe81b4961980dba2b1fbad7626eb74a0b20dea68
Alex Bredariol Grilo <alex@fonista.co>
2011-11-08 14:51:13 -0200

    Fix addition for followup questions

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,7 @@
 class SearchesController < ApplicationController
 
   def index
-    if params[:q].blank?
+    if params[:q].blank? && params[:question].blank?
       redirect_to root_path
       return
     end
@@ -10,6 +10,7 @@ class SearchesController < ApplicationController
 
     if ab_test(:new_question_as_search) == :new_search_scheme
       @question = Question.new(params[:q] ? { :title => params[:q] } : {})
+      @question.safe_update(%w[body parent_question_id], params[:question])
       @bing_results = Support::Bing.search(@question.title)
     end
 
