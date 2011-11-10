8af7eeedf26eba1a5ba09024d0d3dea011f1f7c3
André Lima <andre.lima@gmail.com>
2011-11-10 10:38:04 -0200

    Preserve user's identity on sign up experiments.

--- experiments/sign_in_with_facebook.rb
+++ experiments/sign_in_with_facebook.rb
@@ -1,4 +1,5 @@
 ab_test 'Sign in with Facebook' do
   alternatives :regular_sign_up, :facebook_sign_in
   metrics :signed_up_action
+  identify { |c| c.identify_vanity(:preserve_identity => true) }
 end
