7c4d6dc78ecf3922a26eea598e4c829482c9cb21
Murilo Pereira <murilo@murilopereira.com>
2011-11-10 15:23:52 -0200

    Complete question responding helpers experiment.

--- experiments/question_responding_helpers.rb
+++ experiments/question_responding_helpers.rb
@@ -1,5 +1,6 @@
 ab_test 'Question responding helpers' do
   alternatives :none, :google_search_link, :bing_results
   metrics :question_posted
+  complete_if { true }
   identify { |c| c.identify_vanity }
 end
