c55a398886da45170f5e21df7f07b8c6fd7c3130
André Lima <andre.lima@gmail.com>
2011-11-21 10:49:20 -0200

    Decouple vanity from identity cookie.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < ActionController::Base
       if current_user.tracked?
         current_user
       else
-        set_vanity_cookie(Umamao::UntrackedUser.instance.id)
+        set_identity_cookie(Umamao::UntrackedUser.instance.id)
         Umamao::UntrackedUser.instance
       end
     end
@@ -43,15 +43,30 @@ class ApplicationController < ActionController::Base
   #
   # Options:
   # exclude_guests: don't track non-logged visitors _at all_.
-  def identify_vanity(options = {})
+  def identifier(options = {})
     if (identity = _vanity_identity) && !options[:preserve_identity]
       identity.id
     elsif options[:exclude_guests]
       Umamao::UntrackedUser.instance.id
     else
-      set_vanity_cookie(SecureRandom.hex(16)) unless cookies[:vanity_id]
-      cookies[:vanity_id]
+      obtain_identity_cookie
     end
+  end
+
+  # If there's a logged user, should track according to User#tracked?,
+  # otherwise should track if the cookie is not that of an untracked user.
+  def should_track?
+    if logged_in?
+      current_user.tracked?
+    else
+      obtain_identity_cookie != Umamao::UntrackedUser.instance.id
+    end
+  end
+  helper_method :should_track?
+
+  def obtain_identity_cookie
+    set_identity_cookie(SecureRandom.hex(16)) unless cookies[:identity]
+    cookies[:identity]
   end
 
   protected
@@ -352,9 +367,7 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def set_vanity_cookie(value)
-    unless cookies[:vanity_id] == value
-      cookies[:vanity_id] = { :value => value, :expires => Time.now + 10.years }
-    end
+  def set_identity_cookie(value)
+    cookies[:identity] = { :value => value, :expires => Time.now + 2.weeks }
   end
 end
--- experiments/question_responding_helpers.rb
+++ experiments/question_responding_helpers.rb
@@ -2,5 +2,5 @@ ab_test 'Question responding helpers' do
   alternatives :none, :google_search_link, :bing_results
   metrics :question_posted
   complete_if { true }
-  identify { |c| c.identify_vanity }
+  identify { |c| c.identifier }
 end
